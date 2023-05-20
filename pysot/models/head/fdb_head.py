cd9dfa23bd0c48e5dfb9cc94bf9adf93b8436552
Huiyiqianli <32036062+Giveupfree@users.noreply.github.com>
2023-05-20 23:05:28 +0800

    Update fdb_head.py

--- pysot/models/head/fdb_head.py
+++ pysot/models/head/fdb_head.py
@@ -4,23 +4,6 @@ import math
 from pysot.core.config import cfg
 import torch.nn.functional as F
 
-class Scale(nn.Module):
-    """A learnable scale parameter.
-
-    This layer scales the input by a learnable factor. It multiplies a
-    learnable scale parameter of shape (1,) with input of any shape.
-
-    Args:
-        scale (float): Initial value of scale factor. Default: 1.0
-    """
-
-    def __init__(self, scale=1.0):
-        super(Scale, self).__init__()
-        self.scale = nn.Parameter(torch.tensor(scale, dtype=torch.float))
-
-    def forward(self, x):
-        return x * self.scale
-
 class FDBHead(torch.nn.Module):
     def __init__(self, cfg, in_channels, add_mean=True):
         """
