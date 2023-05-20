a9ea09e2f0cb73f387158e3ad0898ee70baed846
Huiyiqianli <32036062+Giveupfree@users.noreply.github.com>
2023-05-20 23:11:52 +0800

    Add files via upload

---
+++ pysot/tracker/base_tracker.py
@@ -0,0 +1,114 @@
+# Copyright (c) SenseTime. All Rights Reserved.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import cv2
+import numpy as np
+import torch
+
+from pysot.core.config import cfg
+import torch.nn as nn
+from torchvision.transforms import transforms as T
+
+class BaseTracker(object):
+    """ Base tracker of single objec tracking
+    """
+    def init(self, img, bbox):
+        """
+        args:
+            img(np.ndarray): BGR image
+            bbox(list): [x, y, width, height]
+                        x, y need to be 0-based
+        """
+        raise NotImplementedError
+
+    def track(self, img):
+        """
+        args:
+            img(np.ndarray): BGR image
+        return:
+            bbox(list):[x, y, width, height]
+        """
+        raise NotImplementedError
+
+
+class SiameseTracker(BaseTracker):
+    def __init__(self):
+        self.image_normalize = T.Compose([
+            T.ToTensor(),
+            # Converts a numpy.ndarray (H x W x C) in the range [0, 255] to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0]
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    def get_subwindow(self, im, pos, model_sz, original_sz, avg_chans):
+        """
+        args:
+            im: bgr based image
+            pos: center position
+            model_sz: exemplar size
+            s_z: original size
+            avg_chans: channel average
+        """
+        if isinstance(pos, float):
+            pos = [pos, pos]
+        sz = original_sz
+        sz = max(sz, 1)
+        im_sz = im.shape
+        c = (original_sz + 1) / 2
+        # context_xmin = round(pos[0] - c) # py2 and py3 round
+        context_xmin = np.floor(pos[0] - c + 0.5)
+        context_xmax = context_xmin + sz - 1
+        # context_ymin = round(pos[1] - c)
+        context_ymin = np.floor(pos[1] - c + 0.5)
+        context_ymax = context_ymin + sz - 1
+        left_pad = int(max(0., -context_xmin))
+        top_pad = int(max(0., -context_ymin))
+        right_pad = int(max(0., context_xmax - im_sz[1] + 1))
+        bottom_pad = int(max(0., context_ymax - im_sz[0] + 1))
+
+        context_xmin = context_xmin + left_pad
+        context_xmax = context_xmax + left_pad
+        context_ymin = context_ymin + top_pad
+        context_ymax = context_ymax + top_pad
+
+        r, c, k = im.shape
+        if any([top_pad, bottom_pad, left_pad, right_pad]):
+            size = (r + top_pad + bottom_pad, c + left_pad + right_pad, k)
+            te_im = np.zeros(size, np.uint8)
+            te_im[top_pad:top_pad + r, left_pad:left_pad + c, :] = im
+            if top_pad:
+                te_im[0:top_pad, left_pad:left_pad + c, :] = avg_chans
+            if bottom_pad:
+                te_im[r + top_pad:, left_pad:left_pad + c, :] = avg_chans
+            if left_pad:
+                te_im[:, 0:left_pad, :] = avg_chans
+            if right_pad:
+                te_im[:, c + left_pad:, :] = avg_chans
+            im_patch = te_im[int(context_ymin):int(context_ymax + 1),
+                             int(context_xmin):int(context_xmax + 1), :]
+        else:
+            im_patch = im[int(context_ymin):int(context_ymax + 1),
+                          int(context_xmin):int(context_xmax + 1), :]
+        try:
+            if not np.array_equal(model_sz, original_sz):
+                im_patch = cv2.resize(im_patch, (model_sz, model_sz))
+        except:
+            print(context_xmin)
+            print(context_xmax)
+            print(context_ymin)
+            print(context_ymax)
+            print(top_pad, bottom_pad, left_pad, right_pad)
+            print(model_sz)
+            print(im_patch.shape)
+            exit()
+        # im_patch = self.image_normalize(im_patch)
+
+        im_patch = im_patch.transpose(2, 0, 1)
+        im_patch = im_patch[np.newaxis, :, :, :]
+        im_patch = im_patch.astype(np.float32)
+        im_patch = torch.from_numpy(im_patch)
+        if cfg.CUDA:
+            im_patch = im_patch.cuda()
+        return im_patch
